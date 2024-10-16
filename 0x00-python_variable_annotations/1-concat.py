c17c0a82b431fca4fc32641520eebe1769f8bb1a
kal-kyokya <kalkyokya4@gmail.com>
2024-10-16 08:11:47 -0700

    Create a type-annotate function that concatenates 2 string inputs.

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+'1-concat' Creates a typed-annotated function that processes 2 string inputs
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenates 2 string inputs."""
+    return (str1 + str2)
