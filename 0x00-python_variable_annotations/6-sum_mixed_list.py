7b2fad71c5c1cbe94b1d8292465a30595217676f
kal-kyokya <kalkyokya4@gmail.com>
2024-10-16 13:44:34 -0700

    Create a type-annotate function that reduces a 'float and int' list into a single element.

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -3,9 +3,9 @@
 '6-sum_mixed_list' Creates a typed annotated function reducing a number list
 """
 from functools import reduce
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[int and float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """Reduces a number list into a float number"""
     return (reduce(lambda x, y: x + y, mxd_lst))
