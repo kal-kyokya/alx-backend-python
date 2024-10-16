911bf1a3ccfe6627e8337320dc06ff8e47eb7fed
kal-kyokya <kalkyokya4@gmail.com>
2024-10-16 08:25:16 -0700

    Create a type-annotate function that generates the string representation of a float.

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+'3-to_str' generates the string representation of a float input
+"""
+
+
+def to_str(n: float) -> str:
+    """Converts a float value to a string"""
+    return (str(n))
