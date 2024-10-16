cc4f43e0c559303bf9aed0933ac365504f263734
kal-kyokya <kalkyokya4@gmail.com>
2024-10-16 07:48:48 -0700

    Use proper typing for async generator.

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,7 +7,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Generates 10 random number between 0 and 10."""
     for _ in range(10):
         await asyncio.sleep(1)
