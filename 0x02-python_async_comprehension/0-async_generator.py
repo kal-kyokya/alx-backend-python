f322c5d72f87b352237f74fde5570b6652480c4a
kal-kyokya <kalkyokya4@gmail.com>
2024-10-16 04:36:53 -0700

    Create an async generator.

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+'0-async_generator' defines a function to be used as integer generator
+"""
+import asyncio
+import random
+
+
+async def async_generator():
+    """Generates 10 random number between 0 and 10."""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
---
+++ 0x02-python_async_comprehension/0-main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+'0-main' is a test file for correct output of an asynchronous generator
+"""
+import asyncio
+
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def print_yielded_values():
+    result = []
+    async for i in async_generator():
+        result.append(i)
+    print(result)
+
+asyncio.run(print_yielded_values())
