ad4e6bd750d3a6a03b3778b38f7d497b41a3e683
kal-kyokya <kalkyokya4@gmail.com>
2024-10-16 08:19:40 -0700

    Create a type-annotate function that floors a float.

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+'2-floor' creates a type-annotated function that processes one integer input
+"""
+
+
+def floor(n: float) -> int:
+    """Extract the whole part of a float value"""
+    return (int(n))
