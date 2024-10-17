b303fa6119fdb01487a2196715c9e9f33fa1a5de
kal-kyokya <kalkyokya4@gmail.com>
2024-10-17 03:40:44 -0700

    Validate an annotated function and apply necessary changes.

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -6,7 +6,7 @@ from typing import List, Tuple
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
-    """"""
+    """Replicates the elements of a tuple a user defined number of times"""
     zoomed_in: List = [
         item for item in lst
         for i in range(int(factor))
