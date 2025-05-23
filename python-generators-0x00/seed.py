c9f62d8ca35c6c7221b747b6dd18873e921dd46c
kal-kyokya <kalkyokya4@gmail.com>
2025-05-23 13:58:50 +0000

    Revamp the 'connect_to_prodev' function which establishes a connection to a Database.

--- python-generators-0x00/seed.py
+++ python-generators-0x00/seed.py
@@ -65,61 +65,33 @@ def create_database(connection):
         print("Creation error: {}".format(err))
     finally:
         cursor.close()
-
+# -----------------------------------------
+# Establish a connection to a Database
+# -----------------------------------------
 def connect_to_prodev():
-    """Connects to the 'ALX_prodev' database in MySQL
+    """Connects to the DB_NAME database in MySQL
     Args:
     	None
     Return:
     	A MySQL Database connection object
     """
-    # Establish a connection the MySQL server
-    conn = connect(
-        user="jpkk",
-        host="localhost",
-        password=mysql_pwd,
-        database="ALX_prodev",
-        allow_local_infile=True,
-    )
+    # Try establishing a connection the MySQL Database
+    try:
+        conn = connect(
+            user=DB_USER
+            host=DB_HOST
+            password=DB_PASSWORD
+            database=DB_NAME
+        )
 
-    # Check if the connection was successful
-    if conn.is_connected():
-        print("Database connection established.\n")
-        return conn
-    else:
+        # Check if the connection was successful
+        if conn.is_connected():
+            print("Database connection established.\n")
+            return conn
+    except Error as err:
         print("Database connection was not established.")
-        return
-
-    class Cursor:
-        """A blueprint for instance of a successfully established database connection"""
-
-        def execute(self, command):
-            """Runs the availed SQL command"""
-            print(command + "\n")
-
-        def fetchone(self):
-            """Retrieves a single record from the Database"""
-            print("Successful retrieval of one record from the database.\n")
-            return True
-
-        def fetchall(self):
-            """Retrieves all the available record in the Database"""
-            print("Successful retrieval of every record in the database.\n")
-            return "Rows of database records.\n"
-
-        def close(self):
-            """Ensures the DML-enabled connection is terminated"""
-            print("Cursor connection closed.\n")
-
-    class DatabaseConn:
-        """A blueprint for instance of a successfully established database connection"""
-
-        def cursor(self):
-            """A DML-enabled object for CRUD operations"""
-            print("Cursor connection established.\n")
-            return Cursor()
-
-    return DatabaseConn()
+        print("Connection to {} error: {}".format(DB_NAME, err))
+        return None
 
 def create_table(connection):
     """Creates a table 'user_data' if it does not exist
