eb45cc53586005e3450ea2e523f95caed6254441
kal-kyokya <kalkyokya4@gmail.com>
2025-05-23 10:30:35 +0000

    Refactor the 'connect' function establishing a connection to the MySQL server.

--- python-generators-0x00/seed.py
+++ python-generators-0x00/seed.py
@@ -2,8 +2,14 @@
 """
 'seed' contains 5 functions enabling set up of the MySQL database 'ALX_prodev' with user data availed in a CSV file
 """
+import os
 from mysql.connector import connect
+from dotenv import load_dotenv
 
+
+load_dotenv()
+
+mysql_pwd = os.getenv("MYSQL_PASSWORD")
 
 def connect_db():
     """Connects to the MySQL database server
@@ -12,23 +18,30 @@ def connect_db():
     Return:
     	A MySQL Server connection object
     """
+    # Establish a connection the MySQL server
     conn = connect(
+        user="jpkk",
         host="localhost",
-        user="jpkk"
+        password=mysql_pwd,
     )
 
-    if conn:
+    # Check if the connection was successful
+    if conn.is_connected():
         print("Server connection established.\n")
 
+        # Create a class allowing remote termination of the server connection
         class CloseConn:
             """Ensures the MySQL Server connection is terminated"""
 
             def close(self):
                 """Executes termination of the MySQL Server connection"""
+                print(conn.close())
                 print("Server connection closed.\n")
 
-                return CloseConn()
-    raise IOError("MySQL server connection was not established.")
+        return CloseConn()
+    else:
+        print("MySQL server connection was not established.")
+        return
 
 def create_database(connection):
     """Creates the database 'ALX_prodev' if it does not exist
