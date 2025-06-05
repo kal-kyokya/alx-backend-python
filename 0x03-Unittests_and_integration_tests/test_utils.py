3484276f4532a45bbdb2a4620b540d94376b4f0d
kal-kyokya <kalkyokya4@gmail.com>
2025-06-05 12:09:25 +0000

    Begin establishment of a parameterized test method asserting 'utils.access_nested_map'.

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+"""
+'test_utils' defines a test method for a test class meant to assert the return values of calls made to 'utils.access_nested_map'
+"""
+import utils
+import unittest
+from parameterized import parameterized
+
+
+access = utils.access_nested_map()
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """A collection of methods aiming to test the functioning of a built-in function.
+    Inheritance:
+    	unittest.TestCase: Class defining assertion functions required for acutal testing.
+    """
+
+    @parameterized.expand(
+        [
+            ("me", {
+                'human': {
+                    'african': {
+                        'congolese': {
+                            'name': 'Jean-Paul De Marie KYOKYA KALULU',
+                        }
+                    }
+                }
+            }, ('human', 'african', 'congolese', 'name')),
+            ("age", {
+                'human': {
+                    'african': {
+                        'congolese': {
+                            'name': 'Jean-Paul De Marie KYOKYA KALULU',
+                            'age': 72,
+                        }
+                    }
+                }
+            }, ('human', 'african', 'congolese', 'age')),
+            ("belief", {
+                'human': {
+                    'african': {
+                        'congolese': {
+                            'name': 'Jean-Paul De Marie KYOKYA KALULU'
+                            'age': 72,
+                            'belief': 'truth',
+                        }
+                    }
+                }
+            }, ('human', 'african', 'congolese', 'belief')),
+            ("value", {
+                'human': {
+                    'african': {
+                        'congolese': {
+                            'name': 'Jean-Paul De Marie KYOKYA KALULU'
+                            'age': 72,
+                            'belief': 'truth',
+                            'value': 'inner-peace',
+                        }
+                    }
+                }
+            }, ('human', 'african', 'congolese', 'value')),
+            ("worldview", {
+                'human': {
+                    'african': {
+                        'congolese': {
+                            'name': 'Jean-Paul De Marie KYOKYA KALULU'
+                            'age': 72,
+                            'belief': 'truth',
+                            'value': 'inner-peace',
+                            'worldview': 'Chaos & Order',
+                        }
+                    }
+                }
+            }, ('human', 'african', 'congolese', 'worldview')),
+        ]
+    )
+    def test_access_nested_map(self):
+        """Test method asserting the return value of 'utils.access_nested_method'
+        Args:
+        	self: Object in which all inherited methods, properties and attributes are stored
+        Return:
+        	Raises an 'Exception' if the test fails, None otherwise
+        """
+        self.assertEqual()
