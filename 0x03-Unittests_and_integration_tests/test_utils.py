66c53f2d14546af902476fdecc20592cee244be8
kal-kyokya <kalkyokya4@gmail.com>
2025-06-06 12:55:22 +0000

    Test out compliance with user requirements.

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,30 +1,34 @@
 #!/usr/bin/env python3
-
 """
-Test suite for utils.py
+'test_utils' defines a test method for a test class meant to assert the return values of calls made to 'utils.access_nested_map'
 """
-
+import utils
 import unittest
-from unittest.mock import patch
 from parameterized import parameterized
-from utils import (
-    access_nested_map,
-    memoize,
-    get_json
-    )
+
+
+access = utils.access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
+    """A collection of methods aiming to test the functioning of a built-in function.
+    Inheritance:
+    	unittest.TestCase: Class defining assertion functions required for acutal testing.
     """
-    Test class for the utils..access_nested_map funtion
-    """
+
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {'b': 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
-    def test_access_nested_map(self, nested_map, path, output):
+    def test_access_nested_map(self, nested_map, path, expected):
+        """Test method asserting the return value of 'utils.access_nested_method'
+        Args:
+        	self: Object in which all inherited methods, properties and attributes are stored
+        	nested_map: The dictionary to be traversed by 'utils.access_nested_map'
+        	path: A tuple guiding traversal of the dictionary
+        	expected: The correct return value upon traversal
+        Return:
+        	Raises an 'Exception' if the test fails, None otherwise
         """
-        Test method
-        """
-        self.assertEqual(access_nested_map(nested_map, path), output)
+        self.assertEqual(access(nested_map, path), expected)
