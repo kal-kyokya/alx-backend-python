1aff842476723ea3c1b62825e380c55e964c4863
kal-kyokya <kalkyokya4@gmail.com>
2025-06-06 12:52:49 +0000

    Test out compliance with user requirements.

---
+++ 0x03-Unittests_and_integration_tests/my_test_utils.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+"""
+'test_utils' defines a test method for a test class meant to assert the return values of calls made to 'utils.access_nested_map'
+"""
+import utils
+import unittest
+from parameterized import parameterized
+
+
+access = utils.access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """A collection of methods aiming to test the functioning of a built-in function.
+    Inheritance:
+    	unittest.TestCase: Class defining assertion functions required for acutal testing.
+    """
+
+    @parameterized.expand(
+        [
+            (
+                "me",
+                {
+                    "human": {
+                        "african": {
+                            "congolese": {
+                                "name": "Jean-Paul De Marie KYOKYA KALULU",
+                            }
+                        }
+                    }
+                },
+                ("human", "african", "congolese", "name"),
+                "Jean-Paul De Marie KYOKYA KALULU"
+            ),
+            (
+                "age",
+                {
+                    "human": {
+                        "african": {
+                            "congolese": {
+                                "name": "Jean-Paul De Marie KYOKYA KALULU",
+                                "age": 72,
+                            }
+                        }
+                    }
+                },
+                ("human", "african", "congolese", "age"),
+                72
+            ),
+            (
+                "belief",
+                {
+                    "human": {
+                        "african": {
+                            "congolese": {
+                                "name": "Jean-Paul De Marie KYOKYA KALULU",
+                                "age": 72,
+                                "belief": "truth",
+                            }
+                        }
+                    }
+                },
+                ("human", "african", "congolese", "belief"),
+                "truth"
+            ),
+            (
+                "value",
+                {
+                    "human": {
+                        "african": {
+                            "congolese": {
+                                "name": "Jean-Paul De Marie KYOKYA KALULU",
+                                "age": 72,
+                                "belief": "truth",
+                                "value": "inner-peace",
+                            }
+                        }
+                    }
+                },
+                ("human", "african", "congolese", "value"),
+                "inner-peace"
+            ),
+            (
+                "worldview",
+                {
+                    "human": {
+                        "african": {
+                            "congolese": {
+                                "name": "Jean-Paul De Marie KYOKYA KALULU",
+                                "age": 72,
+                                "belief": "truth",
+                                "value": "inner-peace",
+                                "worldview": "Chaos & Order",
+                            }
+                        }
+                    }
+                },
+                ("human", "african", "congolese", "worldview"),
+                "Chaos & Order"
+            ),
+        ]
+    )
+    def test_access_nested_map(self, name, nested_map, path, expected):
+        """Test method asserting the return value of 'utils.access_nested_method'
+        Args:
+        	self: Object in which all inherited methods, properties and attributes are stored
+        	name: Assigns the parameterized test case a unique name
+        	nested_map: The dictionary to be traversed by 'utils.access_nested_map'
+        	path: A tuple guiding traversal of the dictionary
+        	expected: The correct return value upon traversal
+        Return:
+        	Raises an 'Exception' if the test fails, None otherwise
+        """
+        self.assertEqual(access(nested_map, path), expected)
--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,114 +1,30 @@
 #!/usr/bin/env python3
+
 """
-'test_utils' defines a test method for a test class meant to assert the return values of calls made to 'utils.access_nested_map'
+Test suite for utils.py
 """
-import utils
+
 import unittest
+from unittest.mock import patch
 from parameterized import parameterized
-
-
-access = utils.access_nested_map
+from utils import (
+    access_nested_map,
+    memoize,
+    get_json
+    )
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """A collection of methods aiming to test the functioning of a built-in function.
-    Inheritance:
-    	unittest.TestCase: Class defining assertion functions required for acutal testing.
     """
-
-    @parameterized.expand(
-        [
-            (
-                "me",
-                {
-                    "human": {
-                        "african": {
-                            "congolese": {
-                                "name": "Jean-Paul De Marie KYOKYA KALULU",
-                            }
-                        }
-                    }
-                },
-                ("human", "african", "congolese", "name"),
-                "Jean-Paul De Marie KYOKYA KALULU"
-            ),
-            (
-                "age",
-                {
-                    "human": {
-                        "african": {
-                            "congolese": {
-                                "name": "Jean-Paul De Marie KYOKYA KALULU",
-                                "age": 72,
-                            }
-                        }
-                    }
-                },
-                ("human", "african", "congolese", "age"),
-                72
-            ),
-            (
-                "belief",
-                {
-                    "human": {
-                        "african": {
-                            "congolese": {
-                                "name": "Jean-Paul De Marie KYOKYA KALULU",
-                                "age": 72,
-                                "belief": "truth",
-                            }
-                        }
-                    }
-                },
-                ("human", "african", "congolese", "belief"),
-                "truth"
-            ),
-            (
-                "value",
-                {
-                    "human": {
-                        "african": {
-                            "congolese": {
-                                "name": "Jean-Paul De Marie KYOKYA KALULU",
-                                "age": 72,
-                                "belief": "truth",
-                                "value": "inner-peace",
-                            }
-                        }
-                    }
-                },
-                ("human", "african", "congolese", "value"),
-                "inner-peace"
-            ),
-            (
-                "worldview",
-                {
-                    "human": {
-                        "african": {
-                            "congolese": {
-                                "name": "Jean-Paul De Marie KYOKYA KALULU",
-                                "age": 72,
-                                "belief": "truth",
-                                "value": "inner-peace",
-                                "worldview": "Chaos & Order",
-                            }
-                        }
-                    }
-                },
-                ("human", "african", "congolese", "worldview"),
-                "Chaos & Order"
-            ),
-        ]
-    )
-    def test_access_nested_map(self, name, nested_map, path, expected):
-        """Test method asserting the return value of 'utils.access_nested_method'
-        Args:
-        	self: Object in which all inherited methods, properties and attributes are stored
-        	name: Assigns the parameterized test case a unique name
-        	nested_map: The dictionary to be traversed by 'utils.access_nested_map'
-        	path: A tuple guiding traversal of the dictionary
-        	expected: The correct return value upon traversal
-        Return:
-        	Raises an 'Exception' if the test fails, None otherwise
+    Test class for the utils..access_nested_map funtion
+    """
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {'b': 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, output):
         """
-        self.assertEqual(access(nested_map, path), expected)
+        Test method
+        """
+        self.assertEqual(access_nested_map(nested_map, path), output)
