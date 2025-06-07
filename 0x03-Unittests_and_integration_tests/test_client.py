5fadac2fcb0d94e640e6efb747b9130acd9d7424
kal-kyokya <kalkyokya4@gmail.com>
2025-06-07 12:40:02 +0000

    Use 'unittest.mock.patch' as a decorator and a context manager to run unit tests.

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -107,3 +107,71 @@ class TestGithubOrgClient(unittest.TestCase):
             new_client = GithubOrgClient('google')
             url = new_client._public_repos_url
             self.assertEqual(url, 'https://api.github.com/orgs/google/repos')
+
+    @patch('client.get_json')
+    def test_public_repos(self, mock_get_json):
+        """
+        Test function for GithubOrgClient.public_repos
+        """
+        mock_get_json.return_value = [
+            {"id": 1936771, "name": "truth"},
+            {"id": 3248507, "name": "ruby-openid-apps-discovery"},
+            {"id": 3248531, "name": "autoparse"},
+            {"id": 3975462, "name": "anvil-build"},
+            {"id": 5072378, "name": "googletv-android-samples"},
+            {"id": 5511393, "name": "ChannelPlate"},
+            {"id": 5753459, "name": "GL-Shader-Validator"},
+            {"id": 5753483, "name": "qpp"},
+            {"id": 5815969, "name": "CSP-Validator"},
+            {"id": 5844236, "name": "embed-dart-vm"},
+            {"id": 6093488, "name": "module-server"},
+            {"id": 6461354, "name": "cxx-std-draft"},
+            {"id": 6461358, "name": "filesystem-proposal"},
+            {"id": 6461369, "name": "libcxx"},
+            {"id": 6601918, "name": "tracing-framework"},
+            {"id": 6694773, "name": "namebench"},
+            {"id": 7411412, "name": "devtoolsExtended"},
+            {"id": 7411424, "name": "sirius"},
+            {"id": 7411426, "name": "testRunner"},
+            {"id": 7411430, "name": "crx2app"},
+            {"id": 7697149, "name": "episodes.dart"},
+            {"id": 7776515, "name": "cpp-netlib"},
+            {"id": 7968417, "name": "dagger"},
+            {"id": 8165161, "name": "ios-webkit-debug-proxy"},
+            {"id": 8459994, "name": "google.github.io"},
+            {"id": 8566972, "name": "kratu"},
+            {"id": 8858648, "name": "build-debian-cloud"},
+            {"id": 9060347, "name": "traceur-compiler"},
+            {"id": 9065917, "name": "firmata.py"},
+            {"id": 9402340, "name": "vector_math.dart"}
+            ]
+
+        with patch(
+            'client.GithubOrgClient._public_repos_url',
+            new_callable=PropertyMock
+        ) as mock_public_repos_url:
+            mock_public_repos_url.return_value = '' \
+                'https://api.github.com/orgs/google/repos'
+
+            new_client = GithubOrgClient('google')
+            repos = new_client.public_repos()
+
+            self.assertListEqual(
+                repos, [
+                    'truth', 'ruby-openid-apps-discovery', 'autoparse',
+                    'anvil-build', 'googletv-android-samples', 'ChannelPlate',
+                    'GL-Shader-Validator', 'qpp', 'CSP-Validator',
+                    'embed-dart-vm', 'module-server', 'cxx-std-draft',
+                    'filesystem-proposal', 'libcxx', 'tracing-framework',
+                    'namebench', 'devtoolsExtended', 'sirius', 'testRunner',
+                    'crx2app', 'episodes.dart', 'cpp-netlib', 'dagger',
+                    'ios-webkit-debug-proxy', 'google.github.io', 'kratu',
+                    'build-debian-cloud', 'traceur-compiler', 'firmata.py',
+                    'vector_math.dart'
+                    ]
+                )
+
+            mock_public_repos_url.assert_called_once()
+            mock_get_json.assert_called_once_with(
+                "https://api.github.com/orgs/google/repos"
+                )
