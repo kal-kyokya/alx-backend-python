fbff1fd8697d991933c756033edc113aba7ad7cf
kal-kyokya <kalkyokya4@gmail.com>
2025-06-08 12:39:54 +0000

    Run a 'test Django project' on 'localhost'.

--- test_django/test_django/urls.py
+++ test_django/test_django/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import BookListCreateAPIView
+from ..my_app.views import BookListCreateAPIView
 
 urlpatterns = [
     path(
