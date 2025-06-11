ba8a612e8fad31716bc8bef9e5d2a3a4eb3f2817
kal-kyokya <kalkyokya4@gmail.com>
2025-06-11 10:32:32 +0000

    Use serializers' 'S. Method Field' and 'Validation Err' to shape and check I/O.

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -1,24 +1,70 @@
+"""
+From the 'serializers module' in the 'rest_framework package' at:
+	/django_env/lib/python3.12/site-packages/rest_framework/serializers.py
+
+Serializers and ModelSerializers are similar to Forms and ModelForms.
+Unlike forms, they are not constrained to dealing with HTML output, and
+form encoded input.
+
+Serialization in REST framework is a two-phase process:
+
+1. Serializers marshal between complex types like model instances, and
+python primitives.
+2. The process of marshalling between python primitives and request and
+response content is handled by parsers and renderers.
+"""
 from rest_framework import serializers
-from .models import (User, Message, Conversation)
+from .models import User, Message, Conversation
 
-class UserSerializer(serializers.ModelSerializer):
-    """Enable User model's 'translation' of JSON data to/from APIs
+
+class UserListSerializer(serializers.ModelSerializer):
+    """Enable User model's 'translation'  to/from JSON for API communications
     Inheritance:
-    	serializers.ModelSerializer: Hold required method for actual serialization
+    	serializers.ModelSerializer: Contains the required methodss for actual serialization
     """
+    # For listing/nesting info without sensitive info
+    full_name = serializers.SerializerMethodField()
 
-    model = User
-    fields = (
-        'user_id', 'email',
-        'username', 'first_name',
-        'last_name', 'phone_number'
-    )
+    class Meta:
+        model = User
+        fields = (
+            'user_id', 'email',
+            'username', 'first_name',
+            'last_name', 'phone_number',
+            'full_name'
+        )
 
+        def get_full_name(self, user_obj):
+            """Method shaping 'full_name' when sent client side
+            Args:
+            	self: Representation of the class instance
+            	user_obj: The dict or User Model instance to be processed
+            Return:
+            	A string representation of the user's full name
+            """
+            if user_obj.last_name:
+                return f"{user_obj.first_name} {user_obj.last_name}"
+            else:
+                return f"{user_obj.first_name}"
+
+        def validate_phone_number(self, phone_no):
+            """Method checking compliance of input attribute to internal rule
+            Args:
+            	self: An instanciation of 'UserListSerializer'
+            	phone_no: The input phone meant to be assessed
+            Return:
+            	Raises a 'serializers.ValidationError' if input violates the rule, 'phone_no' otherwise
+            """
+            if not phone_value.startswith('+'):
+                raise serializers.ValidationError(
+                    "Kindly: Phone number must start with a '+' sign"
+                )
+            return phone_no
 
 class MessageSerializer(serializers.ModelSerializer):
     """Enable Message model's 'translation' of JSON data to/from APIs
     Inheritance:
-    	serializers.ModelSerializer: Hold required method for actual serialization
+    	serializers.ModelSerializer: Contains the required methods for actual serialization
     """
 
     model = Message
@@ -32,7 +78,7 @@ class MessageSerializer(serializers.ModelSerializer):
 class ConversationSerializer(serializers.ModelSerializer):
     """Enable Conversation model's 'translation' of JSON data to/from APIs
     Inheritance:
-    	serializers.ModelSerializer: Hold required method for actual serialization
+    	serializers.ModelSerializer: Contains the required methods for actual serialization
     """
 
     model = Conversation
