453e6c08aec25fcc6bdb534c8825e6e6bfa40122
kal-kyokya <kalkyokya4@gmail.com>
2025-06-11 21:58:06 +0000

    Create viewsets for the conversation and message models.

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import ConversationSerializer, MessageSerializer
 
-# Create your views here.
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    """A 'viewset' for handling operations for the conversation model
+    Inheritance:
+    	viewsets.ModelViewSet: Collection of Django predefined CRUD executing methods and required attributes.
+    """
+
+    serializer_class = ConversationSerializer
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    """A 'viewset' for handling operations for the message model
+    Inheritance:
+    	viewsets.ModelViewSet: Collection of Django predefined CRUD executing methods and required attributes.
+    """
+
+    serializer_class = MessageSerializer
