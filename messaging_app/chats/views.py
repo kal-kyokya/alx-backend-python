7b0e97eea4dee3c89695c0a09e4ca96b49f09ad8
kal-kyokya <kalkyokya4@gmail.com>
2025-06-12 11:34:27 +0000

    Establish a 'GET' request handler for the 'Conversation' view.

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from rest_framework import viewsets, status
+from rest_framework import viewsets, permissions
 from .serializers import ConversationSerializer, MessageSerializer
 from .filters import MessageFilter
 from django_filters.rest_framework import DjangoFilterBackend
+from .permissions import IsParticipantOfConversation
 
 
 class ConversationViewSet(viewsets.ModelViewSet):
@@ -12,6 +13,22 @@ class ConversationViewSet(viewsets.ModelViewSet):
     """
 
     serializer_class = ConversationSerializer
+    authentication_class = [JWTAuthentication]
+    permission_classes = [
+        permissions.IsAuthenticated,
+        IsParticipantOfConversation
+    ]
+
+    def get_queryset(self):
+        """Ensures a user only sees conversations they are a participant of
+        Args:
+        	self: An object representation of the class instance
+        return:
+        	Every conversations the user participate in, or None
+        """
+        if self.request.user.is_authenticated:
+            return self.request.user.conversations.all().distinct()
+        return Conversation.objects.none()
 
 
 class MessageViewSet(viewsets.ModelViewSet):
