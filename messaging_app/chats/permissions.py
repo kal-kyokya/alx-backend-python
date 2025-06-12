7c50a0da5bc057bc65a5de547441c4dd33445242
kal-kyokya <kalkyokya4@gmail.com>
2025-06-12 11:12:32 +0000

    Use 'BasePermission' from 'rest_framework.permissions' to complete authorization.

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -2,9 +2,10 @@ from rest_framework import permissions
 
 
 class ISSenderOrReadOnly(permissions.BasePermission):
-    """Allow only participants in a conversation to send,
-    view, update and delete messages.
+    """Allow only participants in a conversation to send, view, update and delete messages.
     Read-only access is allowed for authenticated user.
+    Inheritance:
+    	permissions.BasePermission: Enable access to authorization facilitating methods and attributes
     """
 
     message = "You do not have permission to perform this action"
@@ -25,3 +26,29 @@ class ISSenderOrReadOnly(permissions.BasePermission):
         # Write permissions are only allowed to sender of message
         # Can perform POST, PUT, PATCH and DELETE
         return message_obj.sender == request.user
+
+
+class IsParticipantOfConversation(permissions.BasePermission):
+    """Allows only authenticated users to access the api/view and manage conversations
+    Inheritance:
+    	permissions.BasePermission: Enable access to authorization facilitating methods and attributes
+    """
+
+    def has_object_permission(self, request, view, conversation_obj):
+        """Ensures user is listed as participants of a conversation
+        Args:
+        	self: A representation of the class instance
+        	request: The object sent by the user as request
+        	view: The function operating at the logic layer
+        	conversation_obj: The conversation instance sent by a user
+        """
+        return request.user in conversation_obj.participants.all()
+
+    def has_permission(self, request, view):
+        """For list view, ensure user is authenticated
+        Args:
+        	self: A representation of the class instance
+        	request: The object sent by the user as request
+        	view: The function operating at the logic layer
+        """
+        return request.user and request.user_is_authenticated
