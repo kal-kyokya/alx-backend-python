5223548f5078ad772555ae23971ae030dae5a657
kal-kyokya <kalkyokya4@gmail.com>
2025-06-13 13:48:40 +0000

    Ensure the 'time limiting' middleware is in 'settings.MIDDLEWARE'.

--- Django-Middleware-0x03/0x03-MessagingApp-Django/settings.py
+++ Django-Middleware-0x03/0x03-MessagingApp-Django/settings.py
@@ -62,7 +62,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'chats.middleware.RestrictAccessByTimeMiddleware',
 ]
 
 ROOT_URLCONF = 'messaging_app.urls'
