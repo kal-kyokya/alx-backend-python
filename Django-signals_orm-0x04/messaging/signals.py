beab62b0b1ef2c94c1f823e97d950086d1084b48
kal-kyokya <kalkyokya4@gmail.com>
2025-06-14 10:09:06 +0000

    Create a receiver for 'pre_save' operations on the Message model.

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,9 +1,13 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
-from .models import Message, Notification
+from .models import Message, MessageHistory, Notification
 
 
 @receiver(post_save, sender=Message)
 def message_post_save_handler(sender, instance, created, *args, **kwargs):
     if created:
         Notification.objects.create(message="There is a new message")
+
+@receiver(pre_save, sender=Message)
+def message_pre_save_handler(sender, instance, *args, **kwargs):
+    MessageHistory.objects.create(**instance)
